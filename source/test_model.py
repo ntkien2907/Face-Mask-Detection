1df1422bb89e5cbd8ceb24e434926fe94214ef32
ntkien2907 <j13daian@gmail.com>
2021-06-02 15:50:29 +0700

    Add one more class: incorrectly_mask. Redesign Neural Network architecture. Use new dataset

--- source/test_model.py
+++ source/test_model.py
@@ -8,8 +8,8 @@ from keras.models import load_model
 model = load_model("face-mask-model.h5")
 
 # Label and Bounding Box
-results = {0: 'without mask', 1: 'mask'}
-GR_dict = {0: (0,0,255), 1: (0,255,0)}
+results = {0: 'incorrectly mask', 1: 'without mask', 2:'mask'}
+GR_dict = {0: (255,0,0), 1: (0,0,255), 2: (0,255,0)}
 rect_size = 4
 
 # Start video capture
@@ -31,33 +31,34 @@ while True:
     faces = haarcascade.detectMultiScale(
         gray,
         scaleFactor=1.05,
-        minNeighbors=4,
-        minSize=(30,30),
+        minNeighbors=5,
+        minSize=(40,40),
         flags=cv2.CASCADE_SCALE_IMAGE,
     )
     
     for f in faces:
-        x, y, w, h = [v*rect_size for v in f] 
+        x, y, w, h = [v*rect_size for v in f]
         
         face_img = im[y:y+h, x:x+w]
-        rerect_sized = cv2.resize(face_img, (150,150))
+        rerect_sized = cv2.resize(face_img, (128,128))
         normalized = rerect_sized / 255.0
-        reshaped = np.reshape(normalized, (1,150,150,3))
+        reshaped = np.reshape(normalized, (1,128,128,3))
         reshaped = np.vstack([reshaped])
 
         # Predict wearing face-mask or not
         result = model.predict(reshaped)
         label = np.argmax(result, axis=1)[0]
+        print(result)
 
         # Draw bounding box
         cv2.rectangle(im, (x,y), (x+w,y+h), GR_dict[label], 2)
         cv2.rectangle(im, (x,y-40), (x+w,y), GR_dict[label], -1)
 
-        # Label "mask" or "without mask"
+        # Label "mask" or "without mask" or "incorrectly mask"
         cv2.putText(im, results[label], (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255), 2)
     
 
-    cv2.imshow('FACE MASK DETECTION', im)
+    cv2.imshow('FACE-MASK DETECTION', im)
     key = cv2.waitKey(10)
     
     if key == 27: 
--- source/train_model.py
+++ source/train_model.py
@@ -1,64 +1,64 @@
 # Import all libraries and modules required
-from keras.layers.core import Dropout
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dropout, Dense
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from keras.callbacks import Callback
+
+
+# Callback function
+class CustomCallbacks(Callback):
+    def on_epoch_end(self, epoch, logs={}):
+      if(logs.get('acc') > 0.95):
+        print("\n95% accuracy reached")
+        self.model.stop_training = True
 
 
 # Build the neural network
 model = Sequential([
-    Conv2D(32, (3,3), activation='relu', input_shape=(150,150,3)),
+    Conv2D(32, (3,3), activation='relu', input_shape=(128,128,3)),
+    Conv2D(32, (3,3), activation='relu'),
     MaxPooling2D(),
     Conv2D(32, (3,3), activation='relu'),
     MaxPooling2D(),
-    Conv2D(32, (3,3),activation='relu'),
-    MaxPooling2D(),
     Flatten(),
-    Dropout(0.5),
-    Dense(50, activation='relu'),
-    Dense(2, activation='softmax'),
+    Dense(128, activation='relu'),
+    Dense(32, activation='relu'),
+    Dense(3, activation='softmax'),
 ])
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
 print(model.summary())
 
 
 # Image Data Augmentation
-TRAINING_DIR = "dataset/train"
+batch_size = 30
 
-train_datagen = ImageDataGenerator(
-    rescale=1.0/255,
-    rotation_range=40,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-)
-
+TRAINING_DIR = "face-mask-dataset-3classes/train"
+train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=45)
 train_generator = train_datagen.flow_from_directory(
     TRAINING_DIR,
-    batch_size=16,
-    target_size=(150,150),
+    target_size=(128,128),
+    batch_size=batch_size,
+    class_mode='categorical',
 )
 
-VALIDATION_DIR = "dataset/test"
-
-validation_datagen = ImageDataGenerator(rescale=1.0/255)
-
+VALIDATION_DIR = "face-mask-dataset-3classes/test"
+validation_datagen = ImageDataGenerator(rescale=1./255)
 validation_generator = validation_datagen.flow_from_directory(
     VALIDATION_DIR,
-    batch_size=16,
-    target_size=(150,150),
+    target_size=(128,128),
+    batch_size=batch_size,
+    class_mode='categorical',
 )
-
 
 # Train model
 model_saved = model.fit_generator(
     train_generator,
-    epochs=10,
+    steps_per_epoch=2000//batch_size,
+    epochs=20,
     validation_data=validation_generator,
+    callbacks=[CustomCallbacks()],
 )
-
 
 # Save model
 model.save('face-mask-model.h5', model_saved)
